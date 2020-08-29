e0bffa770a3716d6de4007a52299b20640bec7fb
koumake <koumake422@gmail.com>
2020-08-29 11:51:03 +0900

    reservationモデル単体テストコード完了

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Reservation, type: :model do
   describe '#create' do
     before do
-      @reservation = FactoryBot.build(:item)
+      @reservation = FactoryBot.build(:reservation)
     end
 
     context "登録できること" do
@@ -16,51 +16,40 @@ RSpec.describe Reservation, type: :model do
       it "date_timeが空では登録できないこと" do
         @reservation.date_time = nil
         @reservation.valid?
-        expect(@reservation.errors.full_messages).to include("")
+        expect(@reservation.errors.full_messages).to include("Date time can't be blank")
       end
 
       it "numberが空では登録できないこと" do
         @reservation.number = nil
         @reservation.valid?
-        expect(@reservation.errors.full_messages).to include("")
+        expect(@reservation.errors.full_messages).to include("Number can't be blank")
       end
 
       it "lastnameが空では登録できないこと" do
         @reservation.lastname = nil
         @reservation.valid?
-        expect(@reservation.errors.full_messages).to include("")
+        expect(@reservation.errors.full_messages).to include("Lastname can't be blank", "Lastname is invalid")
       end
 
       it "firstnameが空では登録できないこと" do
         @reservation.firstname = nil
         @reservation.valid?
-        expect(@reservation.errors.full_messages).to include("")
+        expect(@reservation.errors.full_messages).to include("Firstname can't be blank", "Firstname is invalid")
       end
 
       it "lastname_kanaが空では登録できないこと" do
         @reservation.lastname_kana = nil
         @reservation.valid?
-        expect(@reservation.errors.full_messages).to include("")
+        expect(@reservation.errors.full_messages).to include("Lastname kana can't be blank", "Lastname kana is invalid")
       end
 
       it "firstname_kanaが空では登録できないこと" do
         @reservation.firstname_kana = nil
         @reservation.valid?
-        expect(@reservation.errors.full_messages).to include("")
+        expect(@reservation.errors.full_messages).to include("Firstname kana can't be blank", "Firstname kana is invalid")
       end
 
-      it "user_idが空では登録できないこと" do
-        @reservation.user_id = nil
-        @reservation.valid?
-        expect(@reservation.errors.full_messages).to include("")
-      end
-
-      it "restaurant_idが空では登録できないこと" do
-        @reservation.restaurant_id = nil
-        @reservation.valid?
-        expect(@reservation.errors.full_messages).to include("")
-      end
     end
 
-
+  end
 end

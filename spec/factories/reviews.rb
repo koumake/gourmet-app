bfebb05654d6bfe8ff6797f0548c3830c38d0059
koumake <koumake422@gmail.com>
2020-08-29 12:08:57 +0900

    reviewモデル単体テストコード実装完了

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -2,8 +2,9 @@ FactoryBot.define do
     factory :review do
        title {'aaa'}
        content {'aaa'}
-       user_id {'2'}
-       restaurant_id {'2'}
-       
+       user_id {2}
+       restaurant_id {2}
+       association :restaurant
+       association :user
     end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  descrive '#create' do
+  describe '#create' do
     before do
       @review = FactoryBot.build(:review)
     end
@@ -13,25 +13,14 @@ RSpec.describe Review, type: :model do
     it "titleが空では登録できないこと" do
       @review.title = nil
       @review.valid?
-      expect(@review.errors.full_messages).to include("")
+      expect(@review.errors.full_messages).to include("Title can't be blank")
     end
 
     it "contentが空では登録できないこと" do
       @review.content = nil
       @review.valid?
-      expect(@review.errors.full_messages).to include("")
+      expect(@review.errors.full_messages).to include("Content can't be blank")
     end
 
-    it "user_idが空では登録できないこと" do
-      @review.user_id = nil
-      @review.valid?
-      expect(@review.errors.full_messages).to include("")
-    end
-
-    it "restaurant_idが空では登録できないこと" do
-      @review.restaurant_id = nil
-      @review.valid?
-      expect(@review.errors.full_messages).to include("")
-    end
   end
 end
